6d95fdb3f76fa82cffa4b264c7d3cefc8237da6e
mallayababu9-hub <kodalimallayababu9@gmail.com>
2025-12-24 00:13:57 +0530

    Change merchant example from 'Amazon' to 'AJIO'

--- schemas.py
+++ schemas.py
@@ -5,7 +5,7 @@ class TransactionRequest(BaseModel):
     user_id: str = Field(example="user_123")
     amount: float = Field(example=2500.75)
     currency: str = Field(example="INR")
-    merchant: str = Field(example="Amazon")
+    merchant: str = Field(example="AJIO")
 
 class TransactionResponse(BaseModel):
     transaction_id: str
